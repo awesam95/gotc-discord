444627fddba335c62763580cd6c9c39c65bc2c27
Sam Karia <kariasamarth@gmail.com>
2018-07-30 05:27:31 +0530

    not, working

--- bot.js
+++ bot.js
@@ -3,6 +3,21 @@ const client = new Discord.Client();
 
 const request = require('request');
 
+function get_compliment() {
+    request('https://spreadsheets.google.com/feeds/list/1eEa2ra2yHBXVZ_ctH4J15tFSGEu-VTSunsrvaCAV598/od6/public/values?alt=json', function(err, response, body) {
+            var data = JSON.parse(body);
+            var rndInt = getRandomInt(0, data.feed.entry.length - 1);
+            var compliment = data.feed.entry[rndInt]['gsx$compliments']['$t'];
+            if(compliment) {
+              res.status(200).send(compliment);
+              var got_compliment = compliment;
+            } else {
+              res.status(404).send("Couldn't retrieve compliment, try again");
+              var got_compliment = 'You are super hot :fire:';
+            }
+          });
+}
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -21,16 +36,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '!compliment') {
-            request('https://spreadsheets.google.com/feeds/list/1eEa2ra2yHBXVZ_ctH4J15tFSGEu-VTSunsrvaCAV598/od6/public/values?alt=json', function(err, response, body) {
-            var data = JSON.parse(body);
-            var rndInt = getRandomInt(0, data.feed.entry.length - 1);
-            var compliment = data.feed.entry[rndInt]['gsx$compliments']['$t'];
-            if(compliment) {
-              message.reply(compliment);
-            } else {
-              message.reply('You are super hot :fire:');
-            }
-          });
+        message.reply(get_compliment());
     }
 });
 
