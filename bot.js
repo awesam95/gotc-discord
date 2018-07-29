63db3075f03d8a7b332c3dcf6df1973fbe054092
Samarth Karia <awesam95@users.noreply.github.com>
2018-07-30 03:59:47 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,46 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+var Discord = require('discord.io');
 
-client.on('ready', () => {
-    console.log('I am ready!');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it needs to execute a command
+    // for this script it will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({ to: channelID, message: 'Pong!' });
+            break;
+            default:
+                bot.sendMessage({ to: channelID, message: 'Unknown command.' });
+        }
+    }
+})
