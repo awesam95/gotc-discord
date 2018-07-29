d75f5467dc56c34d4f3dad03bdeb974abf3e29eb
Sam Karia <kariasamarth@gmail.com>
2018-07-30 04:56:14 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,11 +21,8 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '!compliment') {
-            request('http://www.google.com', function (error, response, body) {
-            console.log('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
-            message.reply('successfull ' + statusCode);
+            request('https://compliment-api.herokuapp.com/', function (error, response, body) {
+            message.reply(body);
         });
     }
 });
