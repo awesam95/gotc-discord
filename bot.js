919f6393bb2e6b514ce85adb317fbe3ee4387a56
Sam Karia <kariasamarth@gmail.com>
2018-07-30 04:54:04 +0530

    Update

--- bot.js
+++ bot.js
@@ -25,6 +25,7 @@ client.on('message', message => {
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             console.log('body:', body); // Print the HTML for the Google homepage.
+            message.reply('successfull ' + statusCode);
         });
     }
 });
