9e67c3d74d0a52b201401a62da200bbfbd0c73d2
Samarth Karia <awesam95@users.noreply.github.com>
2018-07-30 04:42:19 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,14 +19,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '!compliment') {
-        request('https://spreadsheets.google.com/feeds/list/1eEa2ra2yHBXVZ_ctH4J15tFSGEu-VTSunsrvaCAV598/od6/public/values?alt=json', function(err, response, body) {
-    var data = JSON.parse(body);
-    var rndInt = getRandomInt(0, data.feed.entry.length - 1);
-    var compliment = data.feed.entry[rndInt]['gsx$compliments']['$t'];
-    if(compliment) {
-      message.reply(compliment);
-    } else {
-      message.reply("Couldn't retrieve compliment, try again");
+        message.reply('You look super hot :fire:');
     }
 });
 
