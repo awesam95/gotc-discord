b774e50951cb0d47cd96c0cbfa9825d4faf455c8
Sam Karia <kariasamarth@gmail.com>
2018-07-30 05:30:48 +0530

    fingers crossed

--- bot.js
+++ bot.js
@@ -3,21 +3,6 @@ const client = new Discord.Client();
 
 const request = require('request');
 
-function get_compliment() {
-    request('https://spreadsheets.google.com/feeds/list/1eEa2ra2yHBXVZ_ctH4J15tFSGEu-VTSunsrvaCAV598/od6/public/values?alt=json', function(err, response, body) {
-            var data = JSON.parse(body);
-            var rndInt = getRandomInt(0, data.feed.entry.length - 1);
-            var compliment = data.feed.entry[rndInt]['gsx$compliments']['$t'];
-            if(compliment) {
-              res.status(200).send(compliment);
-              var got_compliment = compliment;
-            } else {
-              res.status(404).send("Couldn't retrieve compliment, try again");
-              var got_compliment = 'You are super hot :fire:';
-            }
-          });
-}
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -36,7 +21,9 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '!compliment') {
-        message.reply(get_compliment());
+            request("https://compliment-api.herokuapp.com", function(error, response, body) {
+              message.reply(body);
+            });
     }
 });
 
